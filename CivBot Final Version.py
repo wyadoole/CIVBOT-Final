daa6d684e90892339152bec9cadb744353b3222a
wyadoole <38680025+wyadoole@users.noreply.github.com>
2019-04-03 23:53:05 -0700

    Final version wiht comments

--- CivBot Final Version.py
+++ CivBot Final Version.py
@@ -24,7 +24,7 @@ conversation_General = [
     "Goodbye",
     "See you later",
 ]
-
+# a  differant topic for the AI chatbot to use
 conversation_Movie = [
     "Hello",
     "Hello There",
@@ -37,83 +37,62 @@ conversation_Movie = [
     "General Kenobi",
 ]
 
-# continues the loop until user ends the program by pressing the X button in the top corner
-
-myList1 = conversation_General
-def listReset(myList1):
-    for index, value in enumerate(myLit):
-        if value is not 0:
-            myList[index] *= 2
-        else:
-            myList[index] = 'zero'
-
-    return myList1
-
-myList2 = conversation_Movie
-def listReset(myList2):
-    for index, value in enumerate(myLit):
-        if value is not 0:
-            myList[index] *= 2
-        else:
-            myList[index] = 'zero'
-
-    return myList2
 
 
 
 # training the chatbots with testing data to get the bots to function correctly #
-trainer_bob=ListTrainer(chatbot_bob)
-trainer_fred=ListTrainer(chatbot_fred)
-trainer_sue=ListTrainer(chatbot_sue)
+trainer_bob=ListTrainer(chatbot_bob) # trainer for bob
+trainer_fred=ListTrainer(chatbot_fred) # trainer for fred
+trainer_sue=ListTrainer(chatbot_sue) #trainer
 trainer_william=ListTrainer(chatbot_william)
 trainer_matt=ListTrainer(chatbot_matt)
 
 # Train #
-trainer_bob.train(conversation_Movie)
-trainer_sue.train(conversation_Movie)
-trainer_matt.train(conversation_General)
-trainer_william.train(conversation_General)
-trainer_fred.train(conversation_General)
-trainer_fred.train(conversation_Movie)
+trainer_bob.train(conversation_Movie)# calls the movie converstation for bob -> to train
+trainer_sue.train(conversation_Movie)# calls the movie converstation for sue -> to train
+trainer_matt.train(conversation_General) # calls the general converstation for matt -> to train
+trainer_william.train(conversation_General)# calls the general conversation for willaim -> to train
+trainer_fred.train(conversation_General)#  calls the general conversation for fred -> to train
+trainer_fred.train(conversation_Movie)# calls the movie conversation for fred -> to train
 
 ################################################################################
 #########################   Conversation of Chatbots    ########################
 ### convesations that the Chat bot uses to make a conversation with the user ###
 ################################################################################
 
-bob_response = ("Hello there")
-print("BOB:",bob_response)
+bob_response = ("Hello there") # greattings for chatbot application
+print("BOB:",bob_response)# prints bob response
 
-while True:
+while True: # if true run loop until no responses are left
 
 
-    fred_response = chatbot_fred.get_response(bob_response)
-    bob_response = chatbot_bob.get_response(fred_response)
+    fred_response = chatbot_fred.get_response(bob_response)# fred bots having converstation with bob about
+    bob_response = chatbot_bob.get_response(fred_response) #gets bobs response so that fred can respond to it
     print("Fred responding to Bob:",fred_response)
     fred_response = conversation_Movie[randint(0, len(conversation_Movie))]
 
 
-    sue_response = chatbot_sue.get_response(fred_response)
-    fred_response = chatbot_fred.get_response(sue_response)
-    print("SUE responding to Fred:",sue_response)
-    sue_response = conversation_Movie[randint(0,len(conversation_Movie))]
+    sue_response = chatbot_sue.get_response(fred_response)# sues bots having converstation with fred about
+    fred_response = chatbot_fred.get_response(sue_response)# gets freds response so that sue can respond to fred
+    print("SUE responding to Fred:",sue_response) #prints sues response
+    sue_response = conversation_Movie[randint(0,len(conversation_Movie))] # calls the list movie at the top of script for the chat bot to use as random response
 
 
-    william_response = chatbot_william.get_response(sue_response)
-    sue_response = chatbot_sue.get_response(william_response)
-    print("Willaim says to SUE:",william_response)
-    william_response = conversation_General[randint(0,len(conversation_General))]
+    william_response = chatbot_william.get_response(sue_response)# william bot having a conversation with william  using movie  topic
+    sue_response = chatbot_sue.get_response(william_response) # gets sues response so that willaim can responed to sue
+    print("Willaim says to SUE:",william_response) # prints willaims response
+    william_response = conversation_General[randint(0,len(conversation_General))]# calls the list general at the top of script for the chat bot to use as random response
 
 
-    matt_response = chatbot_matt.get_response(william_response)
-    william_response = chatbot_william.get_response(matt_response)
-    print("Matt says to William:",matt_response)
-    matt_response = conversation_General[randint(0, len(conversation_General))]
+    matt_response = chatbot_matt.get_response(william_response) # matt bot having a conversation with william  using general   topic
+    william_response = chatbot_william.get_response(matt_response)# gets william pervious response so that matt can respond to
+    print("Matt says to William:",matt_response)#prints matts response
+    matt_response = conversation_General[randint(0, len(conversation_General))] # calls the list general at the top of script for the chat bot to use as random response
 
 
-    if  matt_response == "Goodbye":
+    if  matt_response == "Goodbye": # ends the program if user inputs  goodbye
         break
-    if william_response == "Hello There":
+    if william_response == "Hello There": # ends the program if user inputs  goodbye
          print("General Kenobi")
 
 
